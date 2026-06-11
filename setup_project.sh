07c112c57ef1de5b1bd7547b1979bb0f8d7b261d
Bruno-SHEMA <b.shema3@alustudent.com>
2026-06-11 12:51:27 -0700

    Error handling on user input

--- setup_project.sh
+++ setup_project.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 printf "Type Parent Directory Version..."
 read input
+#error handling if the user didnt enter directory name
+if [ -z "$input" ]; then
+printf "Please Provide Parent Directory version..."
+read input
+fi
+if [ -z "$input" ]; then
+echo "Erro: Didn't procide Project name"
+exit 1
+fi
+#command to create a directory
 mkdir attendance_tracker_$input
 echo "Created directory attendance_tracker_$input"
